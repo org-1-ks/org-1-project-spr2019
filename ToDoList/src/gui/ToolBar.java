a4e880d964156c19d59f1e9cc47320a0eb93cc7b
Ozaren <krishna.sd.2012@gmail.com>
2019-04-22 10:39:20 -0700

    added file handling and ported to using the Element interface

--- ToDoList/src/gui/ToolBar.java
+++ ToDoList/src/gui/ToolBar.java
@@ -2,9 +2,12 @@ package gui;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JToolBar;
+
+import controller.Sort;
 
 /**
  * 
@@ -45,6 +48,8 @@ public class ToolBar extends JToolBar
         });
 
         insert.addActionListener(e -> {
+            frame.selectItem();
+            
             JOptionPane.showMessageDialog(frame, "Item inserted", "Button Test", JOptionPane.INFORMATION_MESSAGE);
         });
 
@@ -53,15 +58,55 @@ public class ToolBar extends JToolBar
         });
 
         print.addActionListener(e -> {
-            JOptionPane.showMessageDialog(frame, "List printed", "Button Test", JOptionPane.INFORMATION_MESSAGE);
+            var fileChooser = new JFileChooser();
+            fileChooser.setMultiSelectionEnabled(false);
+            fileChooser.showOpenDialog(frame);
+            var file = fileChooser.getSelectedFile();
+            
+            if(file == null) {
+                JOptionPane.showMessageDialog(frame, "Please select a file", "Print", JOptionPane.ERROR_MESSAGE);
+            } else {
+                frame.list.printTo(file);
+                JOptionPane.showMessageDialog(frame, "List printed", "Print", JOptionPane.INFORMATION_MESSAGE);
+            }
+
+            frame.updateList();
         });
 
         save.addActionListener(e -> {
-            JOptionPane.showMessageDialog(frame, "List saved", "Button Test", JOptionPane.INFORMATION_MESSAGE);
+            var fileChooser = new JFileChooser();
+            fileChooser.setMultiSelectionEnabled(false);
+            fileChooser.showOpenDialog(frame);
+            var file = fileChooser.getSelectedFile();
+            
+            if(file == null) {
+                JOptionPane.showMessageDialog(frame, "Please select a file", "Save", JOptionPane.ERROR_MESSAGE);
+            } else {
+                frame.list.saveTo(file);
+                JOptionPane.showMessageDialog(frame, "List saved", "Save", JOptionPane.INFORMATION_MESSAGE);
+            }
+
+            frame.updateList();
         });
 
         load.addActionListener(e -> {
-            JOptionPane.showMessageDialog(frame, "List loaded", "Button Test", JOptionPane.INFORMATION_MESSAGE);
+            var fileChooser = new JFileChooser();
+            fileChooser.setMultiSelectionEnabled(false);
+            fileChooser.showOpenDialog(frame);
+            var file = fileChooser.getSelectedFile();
+            
+            if(file == null || !file.exists()) {
+                JOptionPane.showMessageDialog(frame, "Please select a file", "Load", JOptionPane.ERROR_MESSAGE);
+            } else {
+                try {
+                    frame.list.loadFrom(file);
+                    JOptionPane.showMessageDialog(frame, "List loaded", "Load", JOptionPane.INFORMATION_MESSAGE);
+                } catch(Exception ex) {
+                    JOptionPane.showMessageDialog(frame, ex.getMessage(), "Load", JOptionPane.ERROR_MESSAGE);   
+                }
+            }
+            
+            frame.updateList();
         });
 
         sort.addActionListener(e -> {
@@ -69,18 +114,25 @@ public class ToolBar extends JToolBar
             
             if("comboBoxChanged".equals(e.getActionCommand())) {
                 String item = (String)sort.getSelectedItem();
+                Sort sort;
                 
                 // TODO: Add sorting mechanisms
                 switch(item) {
                     case PRIORITY:
+                        sort = Sort.Priority;
                         break;
                     case DUE_DATE:
+                        sort = Sort.DueDate;
                         break;
                     case TASK_NAME:
+                        sort = Sort.Name;
                         break;
                     default:
                         throw new IllegalStateException(String.format("Unknown Item: %s", item));
                 }
+                
+                frame.list.sortBy(sort);
+                frame.updateList();
             }
         });
 
